1cd0d4df96811c7e18977f57979ebba28619b7d6
tenomiss <64487131+tenomiss@users.noreply.github.com>
2022-10-21 08:59:43 +0200

    Update ex01_somme_entiers.py

--- ex01_somme_entiers.py
+++ ex01_somme_entiers.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+print("test")
+#import time
 
-import time
 
 def somme_entiers_1(n):
   somme = 0
@@ -8,7 +8,29 @@ def somme_entiers_1(n):
     somme += i
   return somme
 
+
+
+
 def somme_entiers_2(n):
   somme = n*(n+1)//2
   return somme
 
+
+
+
+
+
+temps0 = time.time()
+  
+for i in range(7):
+    
+    
+  n=10^i
+  somme_entiers_1(n)
+  print(n)
+    
+    
+temps1 = time.time()
+    
+print(temps1 - temps0)
+  
